dfbec9c8a00cb878870b38ee591efa8adf683029
techy <himanshu766538@gmail.com>
2026-04-15 18:43:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.getElementById("openBtn").onclick = function() {
     document.getElementById("surprise").classList.remove("hidden");
+
+    // blur remove
+    document.querySelector(".overlay").style.backdropFilter = "none";
+    document.querySelector(".overlay").style.background = "transparent";
+
+    // button hide
     this.style.display = "none";
 }
